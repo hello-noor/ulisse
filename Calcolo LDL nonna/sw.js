56c26e86c63ef477752d1b4e2a558688742e927b
noor <hello@noor.studio>
2026-09-10 18:54:40 +0200

    v3

--- Calcolo LDL nonna/sw.js
+++ Calcolo LDL nonna/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ldl-nonna-v1';
+const CACHE_NAME = 'ldl-nonna-v2';
 const ASSETS = [
   './index.html',
   './manifest.json',
@@ -9,6 +9,16 @@ self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
