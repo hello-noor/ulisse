17fbf35c1ba2678f8c80c605b3556a777f0304b6
noor <hello@noor.studio>
2026-09-26 14:50:54 +0200

    v3

--- ulibricks/sw.js
+++ ulibricks/sw.js
@@ -1,5 +1,5 @@
-const CACHE='ulibricks-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./three.min.js','./icons/icon-192.png','./icons/icon-512.png','./icons/maskable-512.png','./icons/apple-touch-icon.png','./icons/favicon-32.png'];
+const CACHE='ulibricks-v3';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./three.min.js','./jspdf.umd.min.js','./icons/icon-192.png','./icons/icon-512.png','./icons/maskable-512.png','./icons/apple-touch-icon.png','./icons/favicon-32.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
